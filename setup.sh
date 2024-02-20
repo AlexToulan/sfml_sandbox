884838be5dd9372014d639ac02166294320b199d
AlexToulan <alex.toulan@gmail.com>
2024-02-20 13:23:04 -0500

    added test to ensure setup works correctly. C++20 enabled with clang++-17. setup option to set clang-17 as defaut compiler. fix to odd compiler error in cellgrid. merge stratagy might be incorrect.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,18 @@
 
 # cmake
 rm -rf build
+
+LINK_CLANG_17=False
+if $LINK_CLANG_17; then
+  ln -s /usr/bin/clang-17 /usr/bin/clang
+  ln -s /usr/bin/clang++-17 /usr/bin/clang++
+  ln -s /usr/bin/clang-cpp-17 /usr/bin/clang-cpp
+  echo "Setting clang++17 to default compiler"
+else
+  echo "Skipped setting clang-17 and clang++-17 as default clang versions"
+fi
+
+export CC="/usr/bin/clang"
+export CXX="/usr/bin/clang++"
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
