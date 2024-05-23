4fa587c46783c08810e9c0835bcca74f479a22d8
AlexToulan <alex.toulan@gmail.com>
2024-05-23 12:27:13 -0400

    rename Ants

---
+++ src/Games/Ants/Ants.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "GameMode/GameMode.hpp"
+
+class Ants //  : public GameMode
+{
+public:
+  Ants();
+  virtual ~Ants();
+  // virtual void onStart();
+  // virtual void processEvents(sf::Event& event);
+  // virtual void update(float ds);
+  // virtual void render(sf::RenderWindow& window);
+  // virtual void onEnd();
+};
