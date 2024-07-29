9e68a895e144f8e887974adcefd3fe5146da551e
AlexToulan <alex.toulan@gmail.com>
2024-07-29 16:36:53 -0400

    removed ninja and formatting

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ mkdir build
 # set clang version
 export CC="/usr/bin/clang-17"
 export CXX="/usr/bin/clang++-17"
-export CMAKE_GENERATOR=Ninja
 
 # cmake
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
